1a5fa77ba41784cf0a50644c3a7c70e221ba63ad
Kayky Rodrigues <rodrigueskayky2025@gmail.com>
2025-03-31 12:20:49 -0300

    Aprendendo condições

---
+++ EXERCICIOS/aula11/ex007.js
@@ -0,0 +1,5 @@
+var vel = 100
+var velm = 120
+console.log(`O seu carro se move em uma velocidade de ${vel}km/h`)
+if (vel >= velm) { console.log('Você está indo rápido demais! Reduza ou vai receber uma multa') }
+    else { console.log('Continue assim! Você está indo na velocidade permitida')}
