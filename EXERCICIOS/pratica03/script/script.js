d3c06194b57964f9739fc0a35a68b68770fd8d0b
Kayky Rodrigues <rodrigueskayky2025@gmail.com>
2025-04-10 12:18:19 -0300

    Criando um projeto

---
+++ EXERCICIOS/pratica03/script/script.js
@@ -0,0 +1,12 @@
+function contar() {
+    var valini = document.getElementById('txti')
+    var valfim = document.getElementById('txtf')
+    var valpas = document.getElementById('txtp')
+    var inicio = Number(valini.value)
+    var fim = Number(valfim.value)
+    var passo = Number(valpas.value)
+    var res = document.getElementById('res')
+    var c = ''
+
+    while ()
+}
