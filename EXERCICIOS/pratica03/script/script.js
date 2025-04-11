f3a9d249a44ba63e7506edd71cb839d606167e08
Kayky Rodrigues <rodrigueskayky2025@gmail.com>
2025-04-10 21:00:06 -0300

    Exercicio

--- EXERCICIOS/pratica03/script/script.js
+++ EXERCICIOS/pratica03/script/script.js
@@ -1,12 +1,30 @@
 function contar() {
-    var valini = document.getElementById('txti')
-    var valfim = document.getElementById('txtf')
-    var valpas = document.getElementById('txtp')
-    var inicio = Number(valini.value)
-    var fim = Number(valfim.value)
-    var passo = Number(valpas.value)
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtfi')
+    var pas = document.getElementById('txtp')
     var res = document.getElementById('res')
-    var c = ''
-
-    while ()
-}
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0 ) {
+        window.alert('ERRO! Não deixe espaços em branco.')
+    } else {
+        res.innerHTML = 'Contando: </br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+        if (p == 0) {
+            window.alert('Passo invalido!')
+        }
+        } if (i < f) {
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+                }
+                res.innerHTML += `\u{1F3C1}`
+            }  else {
+                for (c = i; c >= f; c -= p) {
+                    res.innerHTML += `${c} \u{1F448}`
+                }
+        }     
+}    
+/* for (c = f; c >= i; c += p) {
+    res.innerHTML += `${c} \u{1F448}`
+}*/
